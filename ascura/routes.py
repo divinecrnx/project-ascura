5cfaa6c0b34373beff418f455cd47c52e4586fe7
divinecrnx <zulhilmi.rasheed98@gmail.com>
2020-04-01 04:54:35 +0800

    Integrated bootstrap and added flash message to layout template. Also created routes and pages for register and login

--- ascura/routes.py
+++ ascura/routes.py
@@ -8,17 +8,21 @@ from ascura.models import Role, School, Course, SPost, SComment, FPost, FComment
 def home():
     return render_template('index.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {matrix} {fn}'.format(matrix=form.matrix.data, fn=form.first_name.data), 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', form=form, title='Register')
 
-@app.route("/register_test")
-def register_test():
-    form = RegistrationForm()
-    return render_template('register_test.html', form=form, title='Register')
-
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form)
