fce9e0cc986a8bbc971dd63d3598f745a39351b6
divinecrnx <zulhilmi.rasheed98@gmail.com>
2020-04-06 13:51:28 +0800

    Added faculty registration form

--- ascura/forms.py
+++ ascura/forms.py
@@ -195,6 +195,29 @@ class SSSRegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
+class FacultyRegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=1, max=30)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=30)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=14)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    role = SelectField('Role', validators=[DataRequired()], choices=\
+        [('4', 'Lecturer'),\
+        ('5', 'HOP'),\
+        ('6', 'Dean')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
